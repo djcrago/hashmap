6ff49b822938c620ff28553d15bab00037ce24ce
Dexter Crago <jodieanddexter@gmail.com>
2024-02-05 11:59:33 -0700

    Create hashSet class
    This class is the same as the hashMap class except it only takes in
    keys and not values. It was much easier to impliment, especially having
    the hashMap base to work off of.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-// whenever you access a bucket through an index
-// if (index < 0 || index >= buckets.length) {
-//   throw new Error('Trying to access index out of bound');
-// }
-
 class HashMap {
   constructor(loadFactor) {
     this.hashMap = [];
@@ -248,33 +243,239 @@ class HashMap {
   }
 }
 
-const hashMap = new HashMap(0.75);
+// Example code of how to create and use a HashMap class
 
-hashMap.set('TestKey', 'TestValue');
-hashMap.set('TestSet', 'TestValueToSet');
-hashMap.set('TestGet', 'TestValueToGet');
-hashMap.set('TestForMul', 'TestGetSecondNode');
-hashMap.set('TestForT', 'TestGetThirdNode');
-hashMap.set('TestRemove', 'TestRemovingANodeAndItsBucket');
-hashMap.set('TestLength', 'TestLengthOfSix');
+// Create a new hash map
+// const hashMap = new HashMap(0.75);
 
-console.log(hashMap.get('TestGet'));
-console.log(hashMap.get('TestForT'));
+// Set key:value pairs on hash map
+// hashMap.set('TestKey', 'TestValue');
+// hashMap.set('TestSet', 'TestValueToSet');
+// hashMap.set('TestGet', 'TestValueToGet');
+// hashMap.set('TestForMul', 'TestGetSecondNode');
+// hashMap.set('TestForT', 'TestGetThirdNode');
+// hashMap.set('TestRemove', 'TestRemovingANodeAndItsBucket');
+// hashMap.set('TestLength', 'TestLengthOfSix');
 
-console.log(hashMap.has('Test'));
+// Get value of some key
+// console.log(hashMap.get('TestGet'));
+// console.log(hashMap.get('TestForT'));
 
-console.log(hashMap.remove('TestKey'));
+// Ask if some key is present in the hash map
+// console.log(hashMap.has('Test'));
 
-console.log(hashMap.length());
+// Remove a key:value pair from the hashmap
+// console.log(hashMap.remove('TestKey'));
 
+// Get the number of keys present throughout the hashmap
+// console.log(hashMap.length());
+
+// Clear the hash map
 // hashMap.clear();
-// console.log(hashMap.hashMap);
-// hashMap.set('TestClear', 'TestForAnActualNewHashMap');
 
-console.log(hashMap.keys());
+// Get an array of all the keys in the hashmap
+// console.log(hashMap.keys());
 
-console.log(hashMap.values());
+// Get an array of all the values in the hashmap
+// console.log(hashMap.values());
 
-console.log(hashMap.entries());
+// Get an array of all [key, value] pair arrays
+// console.log(hashMap.entries());
 
-console.log(hashMap.hashMap);
+// HashSet class is the same as HashMap but without values
+
+class HashSet {
+  constructor(loadFactor) {
+    this.hashMap = [];
+    this.loadFactor = loadFactor;
+  }
+
+  hash(string) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < string.length; i++) {
+      hashCode = (primeNumber * hashCode + string.charCodeAt(i)) % 16;
+    }
+
+    return hashCode;
+  }
+
+  set(key) {
+    const index = this.hash(key);
+
+    if (this.hashMap[index] === undefined) {
+      this.hashMap[index] = {
+        head: {},
+        capacity: 16,
+      };
+    }
+    let bucket = this.hashMap[index];
+    let currentCapacity = bucket.capacity * this.loadFactor;
+
+    let headNode = bucket.head;
+    if (headNode.nextNode === undefined) {
+      const newNode = this.makeNode(key);
+      bucket.head = newNode;
+    } else {
+      let currentNode = bucket.head;
+      let duplicate = false;
+      while (currentNode.nextNode) {
+        let currentKeys = Object.keys(currentNode);
+        currentKeys.forEach((currentKey) => {
+          if (currentKey === key) {
+            duplicate = true;
+          }
+        });
+        currentNode = currentNode.nextNode;
+      }
+
+      if (!duplicate) {
+        currentNode.nextNode = this.makeNode(key, currentNode);
+      }
+
+      const numberOfNodes = this.length();
+
+      if (currentCapacity === numberOfNodes) {
+        bucket.capacity += 8;
+        currentCapacity = bucket.capacity * this.loadFactor;
+      }
+    }
+  }
+
+  has(key) {
+    if (this.findX('node', key)) {
+      return true;
+    }
+    return false;
+  }
+
+  remove(key) {
+    let removed = false;
+
+    const bucketWithNode = this.findX('bucket', key);
+    const bucketIndex = this.hashMap.indexOf(bucketWithNode);
+
+    let nodeWithKey = this.findX('node', key);
+
+    if (nodeWithKey) {
+      // if only node in bucket, remove entire bucket
+      if (nodeWithKey.previousNode === null && nodeWithKey.nextNode === null) {
+        this.hashMap.splice(bucketIndex, 1);
+        removed = true;
+        // if node is head node
+      } else if (nodeWithKey.previousNode === null) {
+        bucketWithNode.head = nodeWithKey.nextNode;
+        removed = true;
+        // if node is tail node
+      } else if (nodeWithKey.nextNode === null) {
+        nodeWithKey.previousNode.nextNode = null;
+        removed = true;
+      } else if (nodeWithKey.previousNode && nodeWithKey.nextNode) {
+        nodeWithKey.previousNode.nextNode = nodeWithKey.nextNode;
+        nodeWithKey.nextNode.previousNode = nodeWithKey.previousNode;
+        removed = true;
+      }
+    }
+
+    return removed;
+  }
+
+  length() {
+    let numberOfKeys = 0;
+
+    const hMap = this.hashMap;
+    hMap.forEach((bucket) => {
+      let currentNode = bucket.head;
+      numberOfKeys += 1;
+      while (currentNode.nextNode) {
+        currentNode = currentNode.nextNode;
+        numberOfKeys += 1;
+      }
+    });
+
+    return numberOfKeys;
+  }
+
+  clear() {
+    this.hashMap = [];
+  }
+
+  keys() {
+    const arrayOfKeys = this.findX('keys');
+
+    return arrayOfKeys;
+  }
+
+  makeNode(key, previousNode = null, nextNode = null) {
+    return { key, previousNode, nextNode };
+  }
+
+  findX(x, key) {
+    let find;
+    if (x === 'node') {
+      find = null;
+    }
+    if (x === 'keys') {
+      find = [];
+    }
+
+    const hMap = this.hashMap;
+    hMap.forEach((bucket) => {
+      let currentNode = bucket.head;
+      let currentKey = currentNode.key;
+      if (x === 'keys') {
+        find.push(currentKey);
+        while (currentNode.nextNode) {
+          currentNode = currentNode.nextNode;
+          currentKey = currentNode.key;
+          find.push(currentKey);
+        }
+      } else if (currentKey === key) {
+        if (x === 'bucket') find = bucket;
+        if (x === 'node') find = currentNode;
+      }
+
+      if (!find) {
+        while (currentNode.nextNode) {
+          currentNode = currentNode.nextNode;
+          currentKey = currentNode.key;
+          if (currentKey === key) {
+            if (x === 'bucket') find = bucket;
+            if (x === 'node') find = currentNode;
+          }
+        }
+      }
+    });
+
+    return find;
+  }
+}
+
+// Example code of how to create and use a HashSet
+
+// Create a new hash map
+// const hashSet = new HashSet(0.75);
+
+// Set keys on hash map
+// hashSet.set('TestKey');
+// hashSet.set('TestSet');
+// hashSet.set('TestForMul');
+// hashSet.set('TestForT');
+// hashSet.set('TestRemove');
+// hashSet.set('TestLength');
+
+// Ask if some key is present in the hash map
+// console.log(hashSet.has('TestForT'));
+
+// Remove a key from the hashmap
+// console.log(hashSet.remove('TestKey'));
+
+// Get the number of keys present throughout the hashmap
+// console.log(hashSet.length());
+
+// Clear the hash map
+// hashSet.clear();
+
+// Get an array of all the keys in the hashmap
+// console.log(hashSet.keys());
